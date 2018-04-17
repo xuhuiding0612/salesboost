29d0aee6ca6f51ec4318067fd4108eb8d504a021
Xuhui Ding <xuhui.ding@hotmail.com>
2018-04-17 00:45:04 -0700

    added dummy data

--- app/src/main/java/com/salesup/salesboost/loader/DummyDataLoader.java
+++ app/src/main/java/com/salesup/salesboost/loader/DummyDataLoader.java
@@ -1,5 +1,6 @@
 package com.salesup.salesboost.loader;
 
+import com.salesup.salesboost.domain.Customer;
 import com.salesup.salesboost.domain.Product;
 import com.salesup.salesboost.repository.CustomerRepository;
 import com.salesup.salesboost.repository.ProductRepository;
@@ -7,15 +8,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
+import java.util.Arrays;
+
 public class DummyDataLoader implements ApplicationRunner {
     @Autowired
     private CustomerRepository customerRepository;
     @Autowired
     private ProductRepository productRepository;
-    @Autowired
-    private
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        Customer customer = new Customer();
+        customer.setName("Xiang Xiang Corgi Dog Supply Corps.");
+        customerRepository.save(customer);
 
+        Product product1 = new Product();
+        product1.setName("cute corgi's butt");
+
+        Product product2 = new Product();
+        product2.setName("short corgi's leg");
+        productRepository.saveAll(Arrays.asList(product1, product2));
     }
 }
