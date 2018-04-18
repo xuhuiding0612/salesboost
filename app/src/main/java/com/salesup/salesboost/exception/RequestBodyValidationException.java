a8e4ea5a017c20e52325b2984c96d48e56b6129e
Xuhui Ding <xuhui.ding@hotmail.com>
2018-04-18 03:16:13 -0700

    Reformatted codes

--- app/src/main/java/com/salesup/salesboost/exception/RequestBodyValidationException.java
+++ app/src/main/java/com/salesup/salesboost/exception/RequestBodyValidationException.java
@@ -4,10 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Request Body Validation Error")
-public class RequestBodyValidationException extends RuntimeException{
+public class RequestBodyValidationException extends RuntimeException {
   public RequestBodyValidationException(String message) {
     super(message);
   }
+
   public RequestBodyValidationException(String message, Throwable throwable) {
     super(message, throwable);
   }
